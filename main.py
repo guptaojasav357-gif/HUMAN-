64c1844f1b9f70d6d32af09141997d5f2a833e2f
guptaojasav357-gif <guptaojasav357@gmail.com>
2026-04-28 08:19:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+#title of app
+st.title("my first streamlit app")
+#adding text
+st.write("hello! creating a simple web application using streamlit library.")
+
+#text output
+name=st.text_input("Enter you name:")
+#number input
+age=st.number_input(Enter your age:")
+
+#display a message when button is clicked
+if st.button("submit"):
+  st.write("hello,{name}!welcome to streamlit.")
